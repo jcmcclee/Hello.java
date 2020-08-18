1ab076cacbdefee795899559e54b4427faaff98e
Jayla McClee <69861005+jcmcclee@users.noreply.github.com>
2020-08-18 12:49:16 -0400

    Add files via upload
    This is the program I created for CMSC 255 Assignment

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/************************************************************* Hello.java
+************************************************************ *Jayla McClee
+*08/17/2020
+* CMSC255- Section 004
+**********************************************************/
+
+public class Hello{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Jayla McClee");
+		System.out.println("Hello.java");
+		System.out.println("Printing out the word");
+		System.out.println("Version 08/17/2020");
+		System.out.println("CMSC255-Section 004");
+		System.out.println();
+	}
+}
